e1bef5f388353d06bfcc3d4e76b6813f57c55645
juamqi <juampikass@gmail.com>
2025-07-09 18:04:46 -0300

    Add post detail view and user authentication to blog
    Implemented a post detail page with its template and URL route. Enhanced the post list template to link to individual posts and display login/logout options based on authentication status. Added Django authentication URLs, login template, and configured login/logout redirects in settings.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.lista_posts, name='lista_posts'),
+    path('post/<int:post_id>/', views.detalles_post, name='detalles_post'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def lista_posts(request):
     posts = Post.objects.all().order_by('-fecha_publicacion')  
     return render(request, 'blog/lista_posts.html', {'posts': posts})
+
+def detalles_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/detalles_post.html', {'post': post})
